08b3963ce2dd99592e68a3904aacba46f3e673fc
FreemanDane <thss15_fengs@163.com>
2017-11-07 14:42:53 +0800

    update package.json

---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import Fundation from "react-foundation"
+import ReactDOM from "react-dom"
+import { Router, Route, Link } from "react-router"
+import Ajax from "react-ajax"
+
+class Login extends Component {
+    render(){
+        
+    }
+}
